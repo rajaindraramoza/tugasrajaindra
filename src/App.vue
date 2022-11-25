dd987512e8e6014b8413c514002af85101e63583
rajaindra97 <rajaindra97@gmail.com>
2022-11-25 19:49:51 +0700

    commit berikutnya

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
 	<div class="container">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#">Navbar</a>
+				<a class="navbar-brand" href="#">Navbarr</a>
 				<button
 					class="navbar-toggler"
 					type="button"
