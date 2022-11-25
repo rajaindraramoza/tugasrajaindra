9a2c26befce3a7cfcc4602e43c7d760447bbd0b8
rajaindra97 <rajaindra97@gmail.com>
2022-11-25 19:43:38 +0700

    memasukkan router ke dalam vueJs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,95 @@
+<script>
+export default {
+	data() {
+		return {
+			dataMahasiswa: [],
+			nama: "",
+			nim: "",
+		};
+	},
+	methods: {
+		simpanData() {
+			var mahasiswa = {
+				nama: this.nama,
+				nim: this.nim,
+			};
+			this.dataMahasiswa.push(mahasiswa);
+		},
+	},
+};
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<router-link to="/" class="nav-link active"
+								>Home</router-link
+							>
+						</li>
+						<li class="nav-item">
+							<router-link class="nav-link" to="/about"
+								>About</router-link
+							>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<router-view />
+		<div class="container">
+			<form>
+				<p class="alert alert-success">Hello RajaIndraRamoza</p>
+				<p>Nama</p>
+				<input v-model="nama" type="text" class="form-text" />
+				<p>Nim</p>
+				<input v-model="nim" type="text" class="form-text" />
+				<div class="mt-2">
+					<button
+						class="btn btn-success"
+						type="submit"
+						@click.prevent="simpanData"
+					>
+						Simpan
+					</button>
+				</div>
+			</form>
+		</div>
+
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Nama</th>
+					<th>Nim</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item in dataMahasiswa" :key="item">
+					<td>{{ item.nama }}</td>
+					<td>{{ item.nim }}</td>
+					<button>x</button>
+					<button @click.prevent="$delete(item, key)">remove</button>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
